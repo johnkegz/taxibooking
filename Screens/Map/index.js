0884c383d334ee024165eb709a6f1da89d04945c
johnkegz <johnkal24@gmail.com>
2019-09-17 11:41:21 +0300

    add googgle maps

--- Screens/Map/index.js
+++ Screens/Map/index.js
@@ -1,14 +1,37 @@
-import React, {Component} from 'react';;
-import {Text, View} from 'react-native';;
+import React, {PureComponent} from 'react';
+import {View, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
-export class Maps extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Map </Text>
-      </View>
-    );
-  }
-}
-
-export default Maps;
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      //   provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      region={{
+        latitude: 0.342885,
+        longitude: 32.591601,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}>
+      <MapView.Marker
+        coordinate={{
+          latitude: 0.342885,
+          longitude: 32.591601,
+        }}
+        pinColor="green"
+      />
+    </MapView>
+  </View>
+);
