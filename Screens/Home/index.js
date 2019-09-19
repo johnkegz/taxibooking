c91d35360324e8a732a96831992499ae851cc747
johnkegz <johnkal24@gmail.com>
2019-09-19 12:49:21 +0300

    add geolocation to get current location

--- Screens/Home/index.js
+++ Screens/Home/index.js
@@ -1,15 +1,36 @@
-import React, {Component} from 'react';;
-import {Button, Text, View} from 'react-native';;
+import React, {Component} from 'react';
+import {Button, Text, View} from 'react-native';
+import Maps from '../Map';
+import Geolocation from 'react-native-geolocation-service';
 
 export class Home extends Component {
+  state = {
+    latitude: 0,
+    longitude: 0,
+    error: null,
+  };
+  componentWillMount() {
+    Geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      error => {
+        this.setState({
+          error: null,
+        });
+      },
+      {maximumAge: 50000, timeout: 20000, enableHighAccuracy: true},
+    );
+  }
   render() {
+    const {latitude, longitude} = this.state;
     return (
       <View>
-        <Button
-          title="go to maps"
-          onPress={() => this.props.navigation.navigate('Maps')}
-        />
-        <Text> home </Text>
+        <Maps latitude={latitude} longitude={longitude} />
       </View>
     );
   }
